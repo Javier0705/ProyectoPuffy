9bfd2e7b7e5f3b23b93b2cff921c5237a5ae9f07
Javier0705 <javar-07@hotmail.com>
2016-07-08 16:19:06 -0300

    Initial commit

---
+++ app/src/main/java/com/javiervargas/proyectopuffy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.javiervargas.proyectopuffy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
